2d660f24586df24f39b951485a5150045bc9f129
Nate Ensign <dixienate@gmail.com>
2022-10-14 10:32:26 -0600

    part 2 complete

--- about.js
+++ about.js
@@ -4,11 +4,20 @@ console.log("hello world");
 
 function handleSubmit(evt) {
 	evt.preventDefault();
-	
-	console.log('form submit');
+	alert('Form has been successfully submitted!')
+	// console.log('form submit');
 }
 
 
 let form = document.querySelector('#contact');
 
 form.addEventListener('submit', handleSubmit);
+
+function complimentDuck(evt) {
+	evt.preventDefault()
+	alert("You're not quite as dumb as you look!")
+}
+
+let niceDuck = document.querySelector('img')
+
+niceDuck.addEventListener('mouseover', complimentDuck)
